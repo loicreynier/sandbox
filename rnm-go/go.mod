b7dfd828ec88fddbfd6317d4c5cc6e0fcbf2f981
Loïc Reynier <loic@loicreynier.fr>
2024-06-02 07:33:14 +0000

    feat(rnm-go): add Git integration

--- rnm-go/go.mod
+++ rnm-go/go.mod
@@ -1,3 +1,5 @@
 module github.com/loicreynier/rnm
 
 go 1.16
+
+require github.com/go-git/go-git/v5 v5.12.0 // indirect
--- rnm-go/rnm.go
+++ rnm-go/rnm.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	git "github.com/go-git/go-git/v5"
 )
 
 func displayUsage() {
@@ -39,17 +41,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	oldFilename := files[0]
-	newFilename := files[1]
-
+	oldFilename, err := filepath.Abs(files[0])
 	dirname := filepath.Dir(oldFilename)
-	newFilename = filepath.Join(dirname, filepath.Base(newFilename))
+	newFilename, err := filepath.Abs(filepath.Join(dirname, files[1]))
 
 	// Check if original file exists
 	os.Stat(oldFilename)
 
 	// Check if the destination file already exists
-	_, err := os.Stat(newFilename)
+	_, err = os.Stat(newFilename)
 	if err == nil && !*force {
 		// File exists, ask for permission to overwrite (good boy)
 		fmt.Printf("File '%s' already exists. Overwrite? (y/n): ", newFilename)
@@ -61,7 +61,55 @@ func main() {
 		}
 	}
 
+	var gitRepo *git.Repository
+	var gitWorktree *git.Worktree
+	var gitStatus git.Status
+	var gitFileStatus *git.FileStatus
+	var oldFilenameRelGit string
+	var newFilenameRelGit string
+	var isGitTracked bool
+
+	isGitTracked = false
+	// Check if file is tracked by Git
+	gitRepo, err = git.PlainOpenWithOptions(
+		dirname,
+		&git.PlainOpenOptions{DetectDotGit: true}, // Look in parent directories for `.git`
+	)
+	if err == nil {
+		gitWorktree, err = gitRepo.Worktree()
+		if err == nil {
+			gitStatus, err = gitWorktree.Status()
+			if err == nil {
+				oldFilenameRelGit, err = filepath.Rel(gitWorktree.Filesystem.Root(), oldFilename)
+				if err != nil {
+					fmt.Printf("Error: Unable to determine relative path of old file: %v\n", err)
+					os.Exit(1)
+				}
+				gitFileStatus = gitStatus[oldFilenameRelGit]
+				if gitFileStatus.Worktree != git.Untracked {
+					isGitTracked = true
+				}
+			}
+		}
+	}
+
 	// Perform the renaming operation
-	os.Rename(oldFilename, newFilename)
+	if isGitTracked {
+		tmpPath, err := filepath.Rel(gitWorktree.Filesystem.Root(), dirname)
+		if err != nil {
+			fmt.Printf("Error: Unable to determine relative path of target: %v\n", err)
+			os.Exit(1)
+		}
+		newFilenameRelGit = filepath.Join(tmpPath, filepath.Base(newFilename))
+
+		_, err = gitWorktree.Move(oldFilenameRelGit, newFilenameRelGit)
+		if err != nil {
+			fmt.Printf("Error: Unable to move file using Git: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		os.Rename(oldFilename, newFilename)
+	}
+
 	fmt.Printf("'%s' -> '%s'", oldFilename, newFilename)
 }
