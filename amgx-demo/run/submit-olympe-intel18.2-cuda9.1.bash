9f1768f0f420e963d8614e76b2b8d0ed5fab818d
Loïc Reynier <loic@loicreynier.fr>
2024-12-03 18:55:24 +0100

    chore(amgx-demo): reorganize and test latest Olympe's NVHPC

--- amgx-demo/run/submit-olympe-intel18.2-cuda9.1.bash
+++ amgx-demo/run/submit-olympe-intel18.2-cuda9.1.bash
@@ -8,8 +8,7 @@
 #SBATCH --mem=16g
 
 module purge
-module load nvidia/nvhpc/22.1-cuda-11.5-majslurm
-module load amgx/2.2.x-nvhpc
+module load intel/18.2 gcc/5.4.0 cuda/9.1.85.3 openmpi/icc/2.0.2.10
 
-make -B
-./bin/amgx.out
+make -B -C .. AMGX_ROOT=/usr/local/amgx/AMGX
+./amgx.out
